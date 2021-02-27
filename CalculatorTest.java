f0f44c2c2d501128824f911b301296b924c1d395
mambadev <mambadevops@gmail.com>
2021-02-27 18:37:04 +0100

    second fubaro

---
+++ CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.mamba.calculator;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+     private Calculator calculator = new Calculator();
+
+     @Test
+     public void testSum() {
+          System.out.println("check test if test :)");
+          assertEquals(5, calculator.sum(2, 3));
+     }
+}
